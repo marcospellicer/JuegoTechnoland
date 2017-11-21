193ae36081d9a336e55206813c884e06453ef84c
alumno <alumno@aula01-09>
2017-11-21 19:22:01 +0100

    fichero de limpiar pantalla

---
+++ AplicacionDeTechnoland/src/aplicaciondetechnoland/LimpiarPantalla.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplicaciondetechnoland;
+
+/**
+ *
+ * @author alumno
+ */
+public class LimpiarPantalla {
+    
+    public static void limpiaPntallas(){
+        System.out.println("\n"+"\n"+"\n"+"\n"+"\n"+"\n"+"\n"+"\n"+"\n"+"\n"+"\n"+"\n"+"\n"+"\n"+"\n"+"\n"+"\n"+"\n"+"\n"+"\n"+"\n"+"\n"+"\n"+"\n"+"\n"+"\n"+"\n"+"\n"+"\n"+"\n"+"\n"+"\n"+"\n"+"\n"+"\n");
+    }
+    
+}
